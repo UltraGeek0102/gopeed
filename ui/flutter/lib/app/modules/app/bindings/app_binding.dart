f0cb5747c5e9a6a4429c0d9f104e8fae4c345aeb
UltraGeek0102 <Dhruv.bosamia@gmail.com>
2026-06-17 11:54:38 +0530

    Update app_binding.dart

--- ui/flutter/lib/app/modules/app/bindings/app_binding.dart
+++ ui/flutter/lib/app/modules/app/bindings/app_binding.dart
@@ -1,10 +1,12 @@
 import 'package:get/get.dart';
 
 import '../controllers/app_controller.dart';
+import '../../../../core/ios_background_download_lifecycle.dart';
 
 class AppBinding extends Bindings {
   @override
   void dependencies() {
+    Get.put(IosDownloadProgressBus());
     Get.lazyPut<AppController>(
       () => AppController(),
     );
